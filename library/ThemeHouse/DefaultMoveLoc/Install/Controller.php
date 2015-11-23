cbe2e2d21b979f925581949f12872ff794b8e518
Jake B <jake.booher@gmail.com>
2015-11-23 10:52:44 -0600

    Initial Commit

---
+++ library/ThemeHouse/DefaultMoveLoc/Install/Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+class ThemeHouse_DefaultMoveLoc_Install_Controller extends ThemeHouse_Install
+{
+
+    protected $_resourceManagerUrl = 'https://xenforo.com/community/resources/default-move-forum.3844/';
+
+    protected $_minVersionId = 1020000;
+
+    protected $_minVersionString = '1.2.0';
+}
